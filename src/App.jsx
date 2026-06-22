c0f807a9e23955dadefaf29ce299a26e16d317fc
Kinjal Joshi <kinjaljoshi@gmail.com>
2026-06-22 13:15:09 +0530

    Router fixes for github

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </>
-    ),
-    {
-      basename: '/reactprojectsetup/',
-    }
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
-  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import "./Main.css";
-import App from './App.jsx'
-import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
-  <>
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </>
-)
+  </HashRouter>
+);
